4abcadbee1856e288bf0ad140c2b7665e1d27814
raul garcia <raulgarefer@gamil.com>
2024-09-30 20:22:58 +0200

    division by 10

---
+++ cpctelera/src/maths/maths.h
@@ -0,0 +1,32 @@
+//-----------------------------LICENSE NOTICE------------------------------------
+//  This file is part of CPCtelera: An Amstrad CPC Game Engine
+//  Copyright (C) 2024 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//-------------------------------------------------------------------------------
+
+//#####################################################################
+//### MODULE: Maths                                                 ###
+//#####################################################################
+//### This module contains mathematical functions.                  ###
+//#####################################################################
+//
+
+#ifndef CPCT_MATHS_H
+#define CPCT_MATHS_H
+
+//Function to divide a given value by 10
+extern   u8 cpct_divideby10 (u8 dividend) __z88dk_fastcall;
+
+#endif
